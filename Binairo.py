c8522891cb3d9cfd5a8adc221922bc430396d2c2
Mahdi Rasouli <mahdirasouli007@gmail.com>
2022-04-20 21:15:55 +0430

    Updating Backtracking Algorithm
    Updating Backtracking Algorithm

--- Binairo.py
+++ Binairo.py
@@ -5,6 +5,11 @@ import Cell
 import heapq
 from MRV_Var import MRV_Var
 from LCV_Var import LCV_Var
+from Constraints import check_for_new_domain
+from Board import Board
+from Print import print_the_board
+from AC3 import ACThree
+
 
 def check_more_than_two_limit(state: State):
     # check rows
@@ -89,9 +94,7 @@ def is_unique(state: State):  # checks if all rows are unique && checks if all c
     return True
 
 
-
-
-def check_circles_limit_heuristic(state: State):
+def check_more_than_two_limit_heuristic(state: State):
     # to check how many constraints are going to make in a row or col for the constraint of number of whites/blacks
 
     count = 0
@@ -117,7 +120,7 @@ def check_circles_limit_heuristic(state: State):
     return count
 
 
-def check_more_than_two_limit_heuristic(state: State):
+def check_circles_limit_heuristic(state: State):
     # to check how many constraints are going to make in a row or col for the constraint of more than 2 same circles
     count = 0
     # check in rows
@@ -204,6 +207,27 @@ def get_unassigned_variables(state: State) -> list:
     return vars
 
 
+def new_domain(state: State):
+    domains = [0 for i in state.board]
+    for q in range(len(domains)):
+        domains[q] = [0 for i in state.board]
+
+    for i in range(0, state.size):
+        for j in range(0, state.size):
+            domains[i][j] = deepcopy(state.board[i][j].domain)
+
+    for i in range(0, state.size):
+        for j in range(0, state.size):
+            if state.board[i][j].value == "_":
+                flag, domain_new = check_for_new_domain(domains, (i,j), state)
+
+                if flag:
+                    domains = domain_new
+                else:
+                    return []
+    return domains
+
+
 def most_constrained_variables(variables: list) -> list:
     # returns a list of most constrained variables
     min_domain_size = math.inf # set to infinity to use in the comparison
@@ -244,66 +268,132 @@ def most_constraining_variable(state:State, most_constrained_variables) -> Cell:
     return heapq.heappop(most_constraining_variables).cell
 
 
-def mrv(state: State) -> Cell:
+def mrv(board: Board) -> tuple:
     # This is Minimum Remaining Value algorithm
     # Which is two factors
     # 1. Most-constrained-variable heuristic
     # which is giving us a list of most constrained variables
     # 2. Most-constraining-variable heuristic
     # which gives us the most constraining variable
-    vars = get_unassigned_variables(state)
+    vars = get_unassigned_variables(board.state)
     most_constrained_vars = most_constrained_variables(vars)
-    most_constraining_var = most_constraining_variable(state, most_constrained_vars)
-    return most_constraining_var
+    most_constraining_var = most_constraining_variable(board.state, most_constrained_vars)
+    return (most_constraining_var.x, most_constraining_var.y)
 
 
-def lcv(cell: Cell, state: State):
+def lcv(board: Board):
     # this is the least constraining value heuristic.
     # it should choose the value in the domain of the current cell to populate
     # which removes least values from other variables' domains
     # but the constraints are not binary, so it's not exactly as LCV.
     lcv_domain = []
+    x, y = board.assigned_variable
+    cell = board.state.board[x][y]
     if cell.value == "_":
-        for domain in cell.domain:
+        for domain in board.domains[x][y]:
             count = 0
             cell.value = domain
-            count += check_circles_limit_heuristic(state)
-            count += check_more_than_two_limit_heuristic(state)
-            count += is_unique_limit_heuristic(state)
+            count += check_circles_limit_heuristic(board.state)
+            count += check_more_than_two_limit_heuristic(board.state)
+            count += is_unique_limit_heuristic(board.state)
+            lcv_var = LCV_Var(deepcopy(cell.value), count)
             cell.value = "_"
-            lcv_var = LCV_Var(cell, count)
             heapq.heappush(lcv_domain, lcv_var)
-        return lcv_domain
+
+
+    if len(board.domains[x][y]) == 0:
+        return 'Failure'
+    else:
+        new_val_from_heap = heapq.heappop(lcv_domain).value
+        board.domains[x][y].remove(new_val_from_heap)
+        return new_val_from_heap
+
+
+
+def set_var_and_value(board: Board, second_value_to_assign):
+    if second_value_to_assign == True:
+        pass
+    else:
+        board.assigned_variable = mrv(board)
+        if not board.assigned_variable:
+            return False
+
+    board.assigned_value = lcv(board)
+    if board.assigned_value != 'Failure':
+        return True
+    else:
+        return False
 
 
 def AC3():
     pass
 
 
-def forward_checking():
+def forward_checking(state: State, domains):
     # this function do a forward checking on the board with the new assignment.
-    # it returns false if the variable's domain is completely deleted after checking / returns true if not.
-    pass
-
-
-def backTrack(state: State):
-    if is_assignment_complete(state):
-        return state
+    # it returns [] if the variable's domain is completely deleted after checking / returns new domain otherwise.
+    flag = True
+    dom = deepcopy(domains)
+    for i in range(0, state.size):
+        for j in range(0, state.size):
+            if state.board[i][j].value == "_":
+                flag, domain_new = check_for_new_domain(dom, (i,j), state)
+                if flag:
+                    dom = domain_new
+                else:
+                    break
+        if not flag:
+            break
+    if flag:
+        return True, dom
     else:
-        X = mrv(state)
-        D = lcv(X, state)
-        while D:
-            val = heapq.heappop(D)
-            X.value = val
-
-            pass
+         return False, []
 
 
+outer_board = None
+def backTrack(b: Board, is_second_value):
+    global outer_board
+    if outer_board == None:
+        outer_board = b
+    else:
+        if b.old == None:
+            return
+
+    if check_termination(b.state):
+        print()
+        print_the_board(b.state, outer_board.state)
+        return
+    else:
+        not_empty = set_var_and_value(b, is_second_value)
+        if not not_empty:
+            backTrack(b.old, False)
+        else:
+            new_vars_domains = deepcopy(b.domains)
+            x, y = b.assigned_variable
+            new_vars_domains[x][y] = b.assigned_value
+            new_state: State = deepcopy(b.state)
+            new_state.board[x][y].value = b.assigned_value
+            status, variables_domain = forward_checking(new_state, new_vars_domains)
+            if status:
+                child_board = Board(new_state, variables_domain)
+                child_board.old = b
+                # going with changed information
+                backTrack(child_board, False)
+            elif not status and len(b.domains[x][y]) == 0:
+                # backtrack with old information..
+                backTrack(b.old, False)
+            else:
+                # next value for the assigned variable
+                backTrack(b.old, True)
+
+
+
+
+# if while loop is done,
 def is_assignment_complete(state: State):  # check if all variables are assigned or not
     for i in range(0, state.size):
         for j in range(0, state.size):
             if state.board[i][j].value == '_':  # exists a variable which is not assigned (empty '_')
-
                 return False
 
     return True
@@ -315,4 +405,3 @@ def is_consistent(state: State):
 
 def check_termination(state: State):
     return is_consistent(state) and is_assignment_complete(state)
-	
